a278a399bb376b57c6473932c4a0d004ed6fd487
au2 <afsin@amida-tech.com>
2014-04-06 23:19:49 -0400

    - first basic test for mri and associated fixes

--- test/records/record.js
+++ test/records/record.js
@@ -14,8 +14,56 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ======================================================================*/
 
-/***
- * Whole master record load and save tests.
- ***/
+var should = require('should');
+var db = require('../../lib/record');
+db.logger.level = 'debug';
 
-var bb = require('../../lib/bluebutton.min.js');
+describe('Basic Test', function() {
+    before(function(done) {
+        db.connection.on('error', function(err) {
+            done(err);
+        });
+        db.connection.on('connected', function() {
+            done();
+        });
+
+        db.connect();
+    });
+    
+    it('Put/Get Master Demographics/Allergies', function(done) {
+        var input = Object.create(null);
+        var inputDemo = input['demographics'] = Object.create(null);
+        inputDemo.data = {section: 'demographics', testkey: {demokey: 'demodata'}};
+        inputDemo.metadata = {metadone: 'metadone', metadtwo: 'metadtwo'};
+        var inputAlle = input['allergies'] = Object.create(null);
+        inputAlle.data = {section: 'allergies', testkey: {allekey: 'alledata'}};
+        inputAlle.metadata = {metaaone: 'metaaone', metaatwo: 'metaatwo'};
+        db.putMaster('testuser', input, {}, function(err) {
+            if (err) {
+                done(err);
+            } else {
+                db.getMaster("testuser", null, function(err, output) {
+                    if (err) {
+                        done(err);
+                    } else {
+                        should(output).have.property('demographics');
+                        should(output).have.property('allergies');
+                        outDemo = output['demographics'];
+                        outAlle = output['allergies'];
+                        should(outDemo).have.property('data');
+                        should(outDemo).have.property('metadata');
+                        should(outDemo.data).have.property('section', 'demographics');
+                        should(outDemo.metadata).have.property('metadone', 'metadone');
+                        should(outDemo.metadata).have.property('metadtwo', 'metadtwo');
+                        done();
+                    }
+                });
+            }
+        });
+    });
+    
+    after(function(done) {
+        db.disconnect();
+        done();
+    });
+});
