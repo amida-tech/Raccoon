0352ff495ab0a4e2b54a5659652e44d7a1e31cb2
au2 <afsin@amida-tech.com>
2014-04-07 20:46:43 -0400

    - configurable section titles - section method tests

--- test/records/record.js
+++ test/records/record.js
@@ -20,10 +20,10 @@ db.logger.level = 'debug';
 
 describe('Basic Test', function() {
     before(function(done) {
-        db.connection.on('error', function(err) {
+        db.once('error', function(err) {
             done(err);
         });
-        db.connection.on('connected', function() {
+        db.once('connected', function() {
             done();
         });
 
@@ -38,7 +38,7 @@ describe('Basic Test', function() {
         var inputAlle = input['allergies'] = Object.create(null);
         inputAlle.data = {section: 'allergies', testkey: {allekey: 'alledata'}};
         inputAlle.metadata = {metaaone: 'metaaone', metaatwo: 'metaatwo'};
-        db.putMaster('testuser', input, {}, function(err) {
+        db.putMaster('testuser', input, null, function(err) {
             if (err) {
                 done(err);
             } else {
@@ -63,7 +63,155 @@ describe('Basic Test', function() {
     });
     
     after(function(done) {
-        db.disconnect();
-        done();
+        db.disconnect(function() {
+            done();
+        });
     });
+});
+
+describe('Section Methods', function() {
+    var testOwner = 'testowner';
+    
+    before(function(done) {
+        db.once('error', function(err) {
+            done(err);
+        });
+        db.once('connected', function() {
+            db.dropAll(function(err, result) {
+                if (err) {
+                    done(err);
+                } else {
+                    var inputRecord = {
+                        'dummysectionfull': {
+                            data: 'expldatafull',
+                            metadata: 'explmetadatafull'
+                        },
+                        'dummysectionremove': {
+                            data: 'expldataremove',
+                            metadata: 'explmetadataremove'
+                        }
+                    };
+                    
+                    db.putMaster(testOwner, inputRecord, null, function(err) {
+                        done(err);
+                    });
+                }
+            });
+        });
+        
+        var options = {
+            ownerTitle: 'testowner',
+            sectionTitles: ['dummysectionadd', 'dummysectionremove', 'dummysectionnull', 'dummysectionfull']
+        };
+        db.connect(null, options);
+    });
+    
+    describe('Add Section Data', function() {
+        it('Verify Empty', function(done) {
+            db.getDummysectionadd(testOwner, function(err, result) {
+                should.not.exist(err);
+                should.not.exist(result);
+                done();
+            });
+        });
+        
+        it('Add Invalid', function(done) {
+            db.putDummysectionadd(testOwner, "invalid data", function(err) {
+                should.exist(err);
+                done();
+            });
+        });
+        
+        it('Add Valid', function(done) {
+            inputRecord = {
+                data: 'expldataadd',
+                metadata: 'explmetadataadd'
+            };
+            db.putDummysectionadd(testOwner, inputRecord, function(err) {
+                should.not.exist(err);
+                done();
+            });
+        });
+        
+       it('Verify Valid', function(done) {
+            db.getDummysectionadd(testOwner, function(err, result) {
+                should.not.exist(err);
+                should.exist(result);
+                should(result).have.property('data', 'expldataadd');
+                should(result).have.property('metadata', 'explmetadataadd');
+                done();
+            });
+       });
+       
+       it('Verify Existing', function(done) {
+           db.getDummysectionfull(testOwner, function(err, result) {
+               should.not.exist(err);
+               should.exist(result);
+               should(result).have.property('data', 'expldatafull');
+               should(result).have.property('metadata', 'explmetadatafull');
+               done();
+           });
+       });
+       
+       it('Verify Existing Null', function(done) {
+           db.getDummysectionnull(testOwner, function(err, result) {
+               should.not.exist(err);
+               should.not.exist(result);
+               done();
+           });
+       });
+    });
+    
+    describe('Remove Section Data', function() {
+        it('Verify Not Empty', function(done) {
+            db.getDummysectionremove(testOwner, function(err, result) {
+                should.not.exist(err);
+                should.exist(result);
+                should(result).have.property('data', 'expldataremove');
+                should(result).have.property('metadata', 'explmetadataremove');
+                done();
+            });
+        });
+        
+        it('Remove', function(done) {
+            db.putDummysectionremove(testOwner, null, function(err) {
+                should.not.exist(err);
+                done();
+            });
+        });
+        
+       it('Verify Removed', function(done) {
+            db.getDummysectionremove(testOwner, function(err, result) {
+                should.not.exist(err);
+                should.not.exist(result);
+                done();
+            });
+       });
+       
+       it('Verify Existing', function(done) {
+           db.getDummysectionfull(testOwner, function(err, result) {
+               should.not.exist(err);
+               should.exist(result);
+               should(result).have.property('data', 'expldatafull');
+               should(result).have.property('metadata', 'explmetadatafull');
+               done();
+           });
+       });
+       
+       it('Verify Existing Null', function(done) {
+           db.getDummysectionnull(testOwner, function(err, result) {
+               should.not.exist(err);
+               should.not.exist(result);
+               done();
+           });
+       });
+    });
+    
+    after(function(done) {
+        db.dropAll(function(err0, result) {
+            db.disconnect(function(err1) {
+                done(err0 || err1);
+            });
+        });
+    });    
 });
